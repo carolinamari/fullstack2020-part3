9b0a123917be8019e5079b29f32f137341c439e6
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-12-11 21:17:41 -0300

    phonebook-backend: step 8

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     {
