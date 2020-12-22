4fdb6e4fdb47170e5f3c0bcd95ce89b1afe221b6
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-12-22 20:35:39 -0300

    command-line database

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.ewwdx.mongodb.net/<dbname>?retryWrites=true&w=majority`
+  
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(p => {
+            console.log(`${p.name} ${p.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Invalid command.')
+    mongoose.connection.close()
+    process.exit(1)
+}
