8b78dcdd5cf23eaec6590d1e70733f4e7d2e43ce
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-12-22 18:16:51 -0300

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
