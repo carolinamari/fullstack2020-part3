9ec46dbbb6c3642d222ef9cf5f4f3888ab260ff9
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-12-23 17:20:51 -0300

    uibuild

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ app.use(express.json())
 morgan.token('data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
+let persons = [
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: '040-123456'
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: '39-44-5323523'
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: '12-43-234345'
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendick',
+        number: '39-23-6423122'
+    }
+]
+
 const generateId = () => {
     return Math.round(Math.random() * 10000)
 }
